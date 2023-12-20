49f8a9000780144c773932f73c0de8ea7c98f743
aniket3765 <ingawale3765@gmail.com>
2023-12-20 15:29:13 +0530

    Adding expence component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (<div>
+        <h2>Expense Items</h2>
+        <div>
+            <ul> 
+                <li>Food Rs 10</li>
+                <li>Petrol Rs 100</li>
+                <li>Movies Rs 200</li>
+            </ul>
+
+        </div>
+    </div>);
+}
+
+export default ExpenseItem;

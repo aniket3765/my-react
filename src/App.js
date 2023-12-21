2c23f7e637e899665af2727082329027016749fd
aniket3765 <ingawale3765@gmail.com>
2023-12-21 14:43:47 +0530

    Updating multiple expences with loop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import ExpenseItem from "./components/ExpenseItem"
 
 function App() {
-  return (
-    <div>
+const data = [
+  {
+    Date:new Date(2023, 12, 20),
+    Title:"Food",
+    Amount:10,
+    Location:"Mumbai"
+},
+
+{
+  Date:new Date(2023, 12, 20),
+  Title:"petrol",
+  Amount:"100",
+  Location:"pune"
+},
+
+{
+  Date:new Date(2023, 12, 20),
+  Title:"loan",
+  Amount:1000,
+  Location:"thane"
+},]
+
+  
+  return ( <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    {data.map((d) =>{return  (<ExpenseItem 
+  Title={d.Title}
+  Amount={d.Amount}
+  Date={d.Date}
+  Location={d.Location}
+  ></ExpenseItem>)
+   })}
+    </div>)
+
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,20 @@
-function ExpenseItem() {
-    return (<div>
-        <h2>Expense Items</h2>
-        <div>
-            <ul> 
-                <li>Food Rs 10</li>
-                <li>Petrol Rs 100</li>
-                <li>Movies Rs 200</li>
-            </ul>
-
+import "./ExpenseItem.css"
+function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <div>{props.Date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.Title}</h2>
+                <h2>{props.Location}</h2>
+                <div className="expense-item__price">Rs{props.Amount}</div>
+            </div>
         </div>
-    </div>);
+        );
 }
 
 export default ExpenseItem;
+
+
+// Food Rs 10</li>
+//                 <li>Petrol Rs 100</li>
+//                 <li>Movies Rs 200</li>

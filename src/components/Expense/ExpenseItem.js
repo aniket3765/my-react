00773729df82c50c321907beb3393459cd37b84b
aniket3765 <ingawale3765@gmail.com>
2023-12-24 04:41:34 +0530

    Adding amount button

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { findDOMNode } from "react-dom";
 function ExpenseItem(props) {
+    const [Amount , setAmount] = useState(props.Amount);
+
+    const AddAmount = () => {
+        setAmount(100);
+    }
 
     const deleteExpense = (e) =>{
         console.log(e.target.parentElement.remove())
@@ -14,8 +19,9 @@ function ExpenseItem(props) {
             <div className="expense-item__description">
                 <h2>{props.Title}</h2>
                 <h2>{props.Location}</h2>
-                <div className="expense-item__price">Rs{props.Amount}</div>
+                <div className="expense-item__price">Rs{Amount}</div>
             </div>
+            <button onClick={AddAmount}>Add 100.Rs</button>
             <button onClick={deleteExpense}>Delete Expense</button>
         </Card>
         );
